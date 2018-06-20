9e511957c0b1c87571fb2951f29f7ef37270f094
cgrossi <cgrossi@gmail.com>
2018-06-20 16:07:16 -0600

    changes

--- js/main.js
+++ js/main.js
@@ -5,5 +5,33 @@ function getUsers () {
     return JSON.parse(request.responseText)
 }
 
+function createRow (index) {
+    const table = document.getElementsByTagName("tbody")[0];
 
-console.log(getUsers());
+    const row = table.insertRow(index);
+
+    return row;
+}
+
+function insertData (row, user) {
+    const idCol = row.insertCell(0);
+    const nameCol = row.insertCell(1);
+    const emailCol = row.insertCell(2);
+
+    idCol.innerHTML = user.id;
+    nameCol.innerHTML = user.name;
+    emailCol.innerHTML = user.email;
+}
+
+function main () {
+    const users = getUsers();
+    let index = 0;
+    for (let i = 0; i < users.length; i++) {
+        const row = createRow(i);
+        const user = users[i];
+        insertData(row, user);
+    }
+}
+
+main();
+
