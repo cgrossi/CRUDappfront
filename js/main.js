9a04d6d6bbe7a7b162e2a0574c7d670d6e140fb0
cgrossi <cgrossi@gmail.com>
2018-06-20 16:54:30 -0600

    changes

--- js/main.js
+++ js/main.js
@@ -13,15 +13,25 @@ function createRow (index) {
 
 
 function insertData (row, user) {
+    const buttonString = `<button id="del-${user.id}"><span class="fa fa-trash glyphicon glyphicon-trash"></span></button>`;
     const idCol = row.insertCell(0);
     const nameCol = row.insertCell(1);
     const emailCol = row.insertCell(2);
 
-    idCol.innerHTML = user.id;
+    idCol.innerHTML = buttonString;
     nameCol.innerHTML = user.name;
     emailCol.innerHTML = user.email;
+
+    document.getElementById(`del-${user.id}`).addEventListener("click", function (){deleteRecord(row, user.id); }, false);
 }
 
+function deleteRecord (row, userid) {
+    row.parentNode.removeChild(row);
+
+    const request = new XMLHttpRequest();
+    request.open("DELETE", "https://crud-app-9987.herokuapp.com/users", false);
+    request.send(JSON.stringify([{id: userid}]));
+}
 
 function main () {
     const users = getUsers();
@@ -34,5 +44,5 @@ function main () {
 }
 
 
-// main();
+main();
 
