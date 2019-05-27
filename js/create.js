bc1242b48e4deb34cdcf14eb6d42d32fc13ee3ad
Carmine <cgrossi@gmail.com>
2019-05-26 18:26:41 -0600

    Fixed formatting

--- js/create.js
+++ js/create.js
@@ -1,35 +1,33 @@
-function main(){
-    // get values user inputs
+function main() {
+        // get values user inputs
 
-    const submit = document.getElementById("submit").onclick = sendHome;
-    const submitcreate = document.getElementById("submitcreate").onclick = sendRefresh;
+        const submit = (document.getElementById('submit').onclick = sendHome);
+        const submitcreate = (document.getElementById('submitcreate').onclick = sendRefresh);
 
-
-    // send as post request
+        // send as post request
 }
 
-function sendRefresh () {
-    sendData();
-    window.location.reload(true);
+function sendRefresh() {
+        sendData();
+        window.location.reload(true);
 }
 
-function sendHome () {
-    sendData();
-    window.location.href="index.html";
-
+function sendHome() {
+        sendData();
+        window.location.href = 'index.html';
 }
 
-function sendData (){
-    const name = document.getElementById("name");
-    const email = document.getElementById("email");
+function sendData() {
+        const name = document.getElementById('name');
+        const email = document.getElementById('email');
 
-    const request = new XMLHttpRequest();
-    request.open("POST", "https://sleepy-hamlet-24092.herokuapp.com/users", false);
-    request.setRequestHeader("Content-type", "application/json");
-    request.send(JSON.stringify([{name: name.value, email: email.value}]));
+        const request = new XMLHttpRequest();
+        request.open('POST', 'https://sleepy-hamlet-24092.herokuapp.com/users', false);
+        request.setRequestHeader('Content-type', 'application/json');
+        request.send(JSON.stringify([{ name: name.value, email: email.value }]));
 
-    name.value = "";
-    email.value = "";
+        name.value = '';
+        email.value = '';
 }
 
 main();
--- js/main.js
+++ js/main.js
@@ -1,49 +1,52 @@
-function getUsers () {
-    const request = new XMLHttpRequest();
-    request.open("GET", "https://sleepy-hamlet-24092.herokuapp.com/users", false);
-    request.send();
-    return JSON.parse(request.responseText)
+function getUsers() {
+        const request = new XMLHttpRequest();
+        request.open('GET', 'https://sleepy-hamlet-24092.herokuapp.com/users', false);
+        request.send();
+        return JSON.parse(request.responseText);
 }
 
-
-function createRow (index) {
-    const table = document.getElementsByTagName("tbody")[0];
-    return table.insertRow(index);
+function createRow(index) {
+        const table = document.getElementsByTagName('tbody')[0];
+        return table.insertRow(index);
 }
 
+function insertData(row, user) {
+        const buttonString = `<button id="del-${
+                user.id
+        }"><span class="fa fa-trash glyphicon glyphicon-trash"></span></button>`;
+        const idCol = row.insertCell(0);
+        const nameCol = row.insertCell(1);
+        const emailCol = row.insertCell(2);
 
-function insertData (row, user) {
-    const buttonString = `<button id="del-${user.id}"><span class="fa fa-trash glyphicon glyphicon-trash"></span></button>`;
-    const idCol = row.insertCell(0);
-    const nameCol = row.insertCell(1);
-    const emailCol = row.insertCell(2);
+        idCol.innerHTML = buttonString;
+        nameCol.innerHTML = user.name;
+        emailCol.innerHTML = user.email;
 
-    idCol.innerHTML = buttonString;
-    nameCol.innerHTML = user.name;
-    emailCol.innerHTML = user.email;
-
-    document.getElementById(`del-${user.id}`).addEventListener("click", function (){deleteRecord(row, user.id); }, false);
+        document.getElementById(`del-${user.id}`).addEventListener(
+                'click',
+                function() {
+                        deleteRecord(row, user.id);
+                },
+                false
+        );
 }
 
-function deleteRecord (row, userid) {
-    row.parentNode.removeChild(row);
+function deleteRecord(row, userid) {
+        row.parentNode.removeChild(row);
 
-    const request = new XMLHttpRequest();
-    request.open("DELETE", "https://sleepy-hamlet-24092.herokuapp.com/users", false);
-    request.setRequestHeader("Content-type", "application/json");
-    request.send(JSON.stringify([{id: userid}]));
+        const request = new XMLHttpRequest();
+        request.open('DELETE', 'https://sleepy-hamlet-24092.herokuapp.com/users', false);
+        request.setRequestHeader('Content-type', 'application/json');
+        request.send(JSON.stringify([{ id: userid }]));
 }
 
-
-function main () {
-    const users = getUsers();
-    for (let i = 0; i < users.length; i++) {
-        const row = createRow(i);
-        const user = users[i];
-        insertData(row, user);
-    }
+function main() {
+        const users = getUsers();
+        for (let i = 0; i < users.length; i++) {
+                const row = createRow(i);
+                const user = users[i];
+                insertData(row, user);
+        }
 }
-
 
 main();
-
